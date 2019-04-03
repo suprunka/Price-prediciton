f6d632a2a6a15af457ac58d8f45e65b6157b0733
Jakub Kida <1067677@ucn.dk>
2019-04-03 21:43:32 +0200

    db connection

--- database/dbConnection.py
+++ database/dbConnection.py
@@ -1,1 +1,23 @@
-from pymongo import Connection
+from pymongo import MongoClient
+import pprint
+
+
+def connect_to_database():
+    db = MongoClient("mongodb+srv://jakub:90809988Qwe@thecluster-zrxzv.mongodb.net/test?retryWrites=true").Houses
+    collection = db.Train
+    return collection
+
+
+def find_one(query):
+    return connect_to_database().find(query)
+
+
+def main():
+    query = {"MSSubClass": "60"}
+    found = find_one(query)
+    pprint.pprint(found)
+
+
+if __name__ == "__main__": main()
+
+
