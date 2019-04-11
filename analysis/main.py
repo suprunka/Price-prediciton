2504721743c75680cb16879eda6bc4eee2414a72
Jakub Kida <1067677@ucn.dk>
2019-04-11 14:31:08 +0200

    prediction

--- analysis/main.py
+++ analysis/main.py
@@ -6,6 +6,7 @@ from pandas import DataFrame
 import datetime
 import os
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error
 import seaborn as sns
 from sklearn.model_selection import StratifiedShuffleSplit
@@ -324,7 +325,6 @@ def get_rid_of_outliers(num_data):
 
 
 #prepare for machine learning
-#housing = strat_train_set.drop(['id'], axis=1)
 housing_labels = strat_train_set["price"].copy()
 
 #Get only numeric values
@@ -366,8 +366,10 @@ def get_labels(data):
 housing_prepared = transform_data(housing)
 housing_labels = get_labels((housing))
 
-# #Select and train a model
-#
+
+
+#Select and train a model
+
 def display_scores(scores):
     print("Scores:", scores)
     print("Mean:", scores.mean())
@@ -407,13 +409,13 @@ def display_scores(scores):
 
 
 
-forest_reg = RandomForestRegressor(n_estimators=10, random_state=42)
-forest_reg.fit(housing_prepared, housing_labels)
-housing_predictions = forest_reg.predict(housing_prepared)
-forest_mse = mean_squared_error(housing_labels, housing_predictions)
-scoresRandomForestRegression =cross_val_score(forest_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
-rforest_rmse_scores = np.sqrt(-scoresRandomForestRegression)
-display_scores(rforest_rmse_scores)
+# forest_reg = RandomForestRegressor(n_estimators=10, random_state=42)
+# forest_reg.fit(housing_prepared, housing_labels)
+# housing_predictions = forest_reg.predict(housing_prepared)
+# forest_mse = mean_squared_error(housing_labels, housing_predictions)
+# scoresRandomForestRegression =cross_val_score(forest_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+# rforest_rmse_scores = np.sqrt(-scoresRandomForestRegression)
+# display_scores(rforest_rmse_scores)
 
 
 
@@ -428,56 +430,36 @@ display_scores(rforest_rmse_scores)
 # display_scores(svr_scores)
 
 
-#
-#
-#
-#
- param_grid = [
-     # try 12 (3×4) combinations of hyperparameters
-     {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
-     # then try 6 (2×3) combinations with bootstrap set as False
-     {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
-   ]
-
- # depths = np.arange(1, 21)
- # num_leafs = [1, 5, 10, 20, 50, 100]
- # param_grid = [{'decisiontreeregressor__max_depth':depths,
- #               'decisiontreeregressor__min_samples_leaf':num_leafs}]
- # pipe_tree = make_pipeline(tree_reg)
-
- # gs = GridSearchCV(estimator=forest_reg, param_grid=param_grid, scoring='neg_mean_squared_error', cv=10)
-#  # gs.fit(housing_prepared, housing_labels)
-#  # cvres = gs.cv_results_
-#  # for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
-#  #    print(np.sqrt(-mean_score), params)
-
-'''
-# <editor-fold>
-# forest_reg = RandomForestRegressor(random_state=42)
-# grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
-#                            scoring='neg_mean_squared_error', return_train_score=True)
-#  grid_search.fit(housing_prepared, housing_labels)
-#  cvres = grid_search.cv_results_
-#  for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
-#      print(np.sqrt(-mean_score), params)
-#  print("#########")
-# </editor-fold>
-# 
-# 
-# <editor-fold>
-#  param_distribs = {
-#          'n_estimators': randint(low=1, high=200),
-#          'max_features': randint(low=1, high=8),
-#      }
-# 
-#  forest_reg = RandomForestRegressor(random_state=42)
-#  rnd_search = RandomizedSearchCV(forest_reg, param_distributions=param_distribs,
-#                                  n_iter=10, cv=5, scoring='neg_mean_squared_error', random_state=42)
-#  rnd_search.fit(housing_prepared, housing_labels)
-#  cvres = rnd_search.cv_results_
-#  for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
-#      print(np.sqrt(-mean_score), params)
-#  </editor-fold
 
 
-'''
+def randmizedSearchCV():
+    param_distribs = {
+        'n_estimators': randint(low=1, high=200),
+        'max_features': randint(low=1, high=8),
+    }
+
+    forest_reg = RandomForestRegressor(random_state=42)
+    rnd_search = RandomizedSearchCV(forest_reg, param_distributions=param_distribs,
+                                    n_iter=10, cv=5, scoring='neg_mean_squared_error', random_state=42)
+    rnd_search.fit(housing_prepared, housing_labels)
+    return rnd_search.cv_results_
+
+def gridSearchCV():
+    param_grid = [
+        # try 12 (3×4) combinations of hyperparameters
+        {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+        # then try 6 (2×3) combinations with bootstrap set as False
+        {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+    ]
+    forest_reg = RandomForestRegressor(random_state=42)
+    grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+                               scoring='neg_mean_squared_error', return_train_score=True)
+    grid_search.fit(housing_prepared, housing_labels)
+    return grid_search.cv_results_
+
+cvres = randmizedSearchCV()
+for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+    print(np.sqrt(-mean_score), params)
+
+
+
