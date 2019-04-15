91a6a3138b22f4c928165754120a9a36fa632edc
Patka <loca.chili@gmail.com>
2019-04-15 15:44:40 +0200

    patrzenie na srednia testu + avg z modeli

--- analysis/main.py
+++ analysis/main.py
@@ -27,6 +27,9 @@ from scipy import stats
 from scipy.special import boxcox1p
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+
 
 
 #AKTUALIZACJA LOL
@@ -249,9 +252,11 @@ def get_labels(data):
     to_return = data_prepared.values
     return to_return
 
-
 housing_prepared = transform_data(housing)
 housing_labels = get_labels((housing))
+test_X= transform_data(strat_test_set)
+test_y = get_labels(strat_test_set)
+
 
 # #Select and train a model
 #
@@ -288,55 +293,120 @@ def gridSearchCV_KNN():
     print('Best Score: ', grid_result.best_score_)
     print('Best Params: ', grid_result.best_params_)
 
+def checkAllModels(models_list):
+    for model in models_list:
+        model.fit(housing_prepared, housing_labels)
+        housing_predictions_train = model.predict(housing_prepared)
+        model.fit(test_X, test_y)
+        housing_predictions_test = model.predict(test_X)
+        scores_train = np.sqrt(-cross_val_score(model, housing_prepared, housing_labels,
+                                                scoring="neg_mean_squared_error", cv=10))
+        scores_test = np.sqrt(-cross_val_score(model, test_X, test_y,
+                                                scoring="neg_mean_squared_error", cv=10))
 
+        display_scores(scores_train, 'Model:'+ model.__class__.__name__)
+        display_scores(scores_test, 'test')
 #LinearRegression
 lin_reg = LinearRegression()
-lin_reg.fit(housing_prepared, housing_labels)
-housing_predictions = lin_reg.predict(housing_prepared)
-
-
-#BayesianRidge
 bayesian_ridge = linear_model.BayesianRidge()
-bayesian_ridge.fit(housing_prepared,housing_labels)
-housing_predictions=bayesian_ridge.predict(housing_prepared)
-bayesian_ridge_scores =cross_val_score(bayesian_ridge, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
-br_scores = np.sqrt(-bayesian_ridge_scores)
-display_scores(br_scores, 'BayesianRidge')
-
-#Ridge
 model_ridge=linear_model.Ridge()
-model_ridge.fit(housing_prepared,housing_labels)
-housing_predictions=model_ridge.predict(housing_prepared)
-scores = cross_val_score(model_ridge, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
-model_ridge_rmse = np.sqrt(-scores)
-display_scores(model_ridge_rmse, 'ridge model')
-
-#DecisionTreeRegressor
 tree_reg = DecisionTreeRegressor(random_state=42)
-tree_reg.fit(housing_prepared, housing_labels)
-housing_predictions = tree_reg.predict(housing_prepared)
-tree_mse = mean_squared_error(housing_labels, housing_predictions)
-tree_rmse = np.sqrt(tree_mse)
-scores = cross_val_score(tree_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
-tree_rmse_scores = np.sqrt(-scores)
-display_scores(tree_rmse_scores, 'DecisionTreeRegressor')
-
-#RandomForestRegressor
 forest_reg =  RandomForestRegressor(n_estimators=50, random_state=42, max_features="auto", max_depth=100,
                                    min_samples_leaf=4, bootstrap=True, min_impurity_decrease=100)
-forest_reg.fit(housing_prepared, housing_labels)
-housing_predictions = forest_reg.predict(housing_prepared)
-forest_mse = mean_squared_error(housing_labels, housing_predictions)
-scoresRandomForestRegression =cross_val_score(forest_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
-rforest_rmse_scores = np.sqrt(-scoresRandomForestRegression)
-display_scores(rforest_rmse_scores,'RandomForestRegressor' )
+knn=neighbors.KNeighborsRegressor(5,weights='uniform')
+models = [lin_reg, bayesian_ridge, model_ridge, tree_reg, forest_reg , knn]
+checkAllModels(models)
+#
+#
+# lin_reg.fit(housing_prepared, housing_labels)
+# housing_predictions = lin_reg.predict(housing_prepared)
+#
+#
+# #BayesianRidge
+# bayesian_ridge = linear_model.BayesianRidge()
+# bayesian_ridge.fit(housing_prepared,housing_labels)
+# housing_predictions=bayesian_ridge.predict(housing_prepared)
+# bayesian_ridge_scores =cross_val_score(bayesian_ridge, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+# br_scores = np.sqrt(-bayesian_ridge_scores)
+# display_scores(br_scores, 'BayesianRidge')
+#
+# #Ridge
+# model_ridge=linear_model.Ridge()
+# model_ridge.fit(housing_prepared,housing_labels)
+# housing_predictions=model_ridge.predict(housing_prepared)
+# scores = cross_val_score(model_ridge, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+# model_ridge_rmse = np.sqrt(-scores)
+# display_scores(model_ridge_rmse, 'ridge model')
+#
+# #DecisionTreeRegressor
+# tree_reg = DecisionTreeRegressor(random_state=42)
+# tree_reg.fit(housing_prepared, housing_labels)
+# housing_predictions = tree_reg.predict(housing_prepared)
+# tree_mse = mean_squared_error(housing_labels, housing_predictions)
+# tree_rmse = np.sqrt(tree_mse)
+# scores = cross_val_score(tree_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+# tree_rmse_scores = np.sqrt(-scores)
+# display_scores(tree_rmse_scores, 'DecisionTreeRegressor')
+#
+# #RandomForestRegressor
+# forest_reg =  RandomForestRegressor(n_estimators=50, random_state=42, max_features="auto", max_depth=100,
+#                                    min_samples_leaf=4, bootstrap=True, min_impurity_decrease=100)
+# forest_reg.fit(housing_prepared, housing_labels)
+# housing_predictions = forest_reg.predict(housing_prepared)
+# forest_mse = mean_squared_error(housing_labels, housing_predictions)
+# scoresRandomForestRegression =cross_val_score(forest_reg, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+# rforest_rmse_scores = np.sqrt(-scoresRandomForestRegression)
+# display_scores(rforest_rmse_scores,'RandomForestRegressor' )
+#
+# # KNN Regression
+# n_neighbors=5
+# knn=neighbors.KNeighborsRegressor(n_neighbors,weights='uniform')
+# knn.fit(housing_prepared,housing_labels)
+# housing_predictions=knn.predict(housing_prepared)
+# scoresKNeighborsRegressor=cross_val_score(knn, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
+# knn_rmse_scores = np.sqrt(-scoresKNeighborsRegressor)
+# display_scores(knn_rmse_scores,'KNeighborsRegressor' )
+#
 
-# KNN Regression
-n_neighbors=5
-knn=neighbors.KNeighborsRegressor(n_neighbors,weights='uniform')
-knn.fit(housing_prepared,housing_labels)
-housing_predictions=knn.predict(housing_prepared)
-scoresKNeighborsRegressor=cross_val_score(knn, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=10)
-knn_rmse_scores = np.sqrt(-scoresKNeighborsRegressor)
-display_scores(knn_rmse_scores,'KNeighborsRegressor' )
+class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, base_models, meta_model, n_folds=5):
+        self.base_models = base_models
+        self.meta_model = meta_model
+        self.n_folds = n_folds
 
+    # We again fit the data on clones of the original models
+    def fit(self, X, y):
+        self.base_models_ = [list() for x in self.base_models]
+        self.meta_model_ = clone(self.meta_model)
+        kfold = KFold(n_splits=self.n_folds, shuffle=True, random_state=156)
+
+        # Train cloned base models then create out-of-fold predictions
+        # that are needed to train the cloned meta-model
+        out_of_fold_predictions = np.zeros((X.shape[0], len(self.base_models)))
+        for i, model in enumerate(self.base_models):
+            for train_index, holdout_index in kfold.split(X, y):
+                instance = clone(model)
+                self.base_models_[i].append(instance)
+                instance.fit(X[train_index], y[train_index])
+                y_pred = instance.predict(X[holdout_index])
+                out_of_fold_predictions[holdout_index, i] = y_pred
+        # Now train the cloned  meta-model using the out-of-fold predictions as new feature
+        self.meta_model_.fit(out_of_fold_predictions, y)
+        return self
+
+    # Do the predictions of all base models on the test data and use the averaged predictions as
+    # meta-features for the final prediction which is done by the meta-model
+    def predict(self, X):
+        meta_features = np.column_stack([
+            np.column_stack([model.predict(X) for model in base_models]).mean(axis=1)
+            for base_models in self.base_models_])
+        return self.meta_model_.predict(meta_features)
+
+def rmsle_cv_(model):
+    cv = KFold(n_splits=5, shuffle=True, random_state=42)
+    score = np.sqrt(- cross_val_score(model, housing_prepared, housing_labels, scoring="neg_mean_squared_error", cv=cv))
+    return(score)
+
+averaged_models = StackingAveragedModels(base_models = (forest_reg,knn,bayesian_ridge), meta_model=model_ridge)
+score = rmsle_cv_(averaged_models)
+print(" Averaged base models score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
