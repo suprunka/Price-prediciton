bf66e3798a14f2ead165b87b9fcc4c4bb65d4b42
Jakub Kida <1067677@ucn.dk>
2019-05-21 17:26:27 +0200

    scheduler

--- app.py
+++ app.py
@@ -55,7 +55,8 @@ def load_user(user_id):
 @app.before_first_request
 def init_scheduler():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(analysis_main.model_preparation, 'interval', hours=24, start_date='2019-05-19 15:25:00')
+    scheduler.add_job(analysis_main.model_preparation, 'interval', minutes=2)
+    # hours = 24, start_date = '2019-05-19 15:25:00'
     scheduler.start()
     atexit.register(lambda: scheduler.shutdown())
 
